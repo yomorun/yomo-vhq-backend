5272aecaf766296771308f7e27110af81a34b42e
Hong Xiaojian <xiaojian.hong@gmail.com>
2021-06-06 21:09:07 +0800

    refactor(socket): send all events to yomo-zipper

--- main.go
+++ main.go
@@ -3,17 +3,38 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 
 	socketio "github.com/googollee/go-socket.io"
-	y3 "github.com/yomorun/y3-codec-golang"
-	"github.com/yomorun/yomo/pkg/quic"
+	"github.com/yomorun/y3-codec-golang"
+	"github.com/yomorun/yomo/pkg/client"
 	"github.com/yomorun/yomo/pkg/rx"
 )
 
+// sender sends the VHQ events to yomo-zipper for stream processing.
+type Sender struct {
+	Stream io.Writer
+}
+
+// EventData represents the event data for broadcasting to all geo-distributed users.
+type EventData struct {
+	Region string `y3:"0x11"`
+	Room   string `y3:"0x12"`
+	Event  string `y3:"0x13"`
+	Data   string `y3:"0x14"`
+}
+
+// Player represents the users in VHQ room.
 type Player struct {
 	ID   string  `json:"id"`
 	Name string  `json:"name"`
@@ -21,6 +42,7 @@ type Player struct {
 	Y    float64 `json:"y"`
 }
 
+// Action represents the user's action in VHQ room.
 type Action struct {
 	ID string  `json:"id"`
 	X  float64 `json:"x"`
@@ -30,52 +52,48 @@ type Action struct {
 var players = make(map[string]Player, 0)
 
 const (
-	socketioRoom   = "yomo-demo"
-	socketioAddr   = "0.0.0.0:19001"
-	sinkServerAddr = "0.0.0.0:4062"
-	zipperAddr     = "localhost:9998"
+	socketioRoom = "yomo-demo"
+	socketioAddr = "0.0.0.0:19001"
+	zipperAddr   = "localhost:9000"
 )
 
-var zipperStream quic.Stream = nil
+var urls = strings.Split(zipperAddr, ":")
+var host = urls[0]
+var port, _ = strconv.Atoi(urls[1])
+var sender *Sender
+var socketioServer *socketio.Server
+var region = os.Getenv("REGION")
 
 func init() {
-	_client, err := quic.NewClient(zipperAddr)
-
-	if err != nil {
-		log.Printf("❌ Connect to the zipper server on %s failure with err: %v", zipperAddr, err)
-		return
+	if region == "" {
+		region = "US"
 	}
-
-	_stream, err := _client.CreateStream(context.Background())
-
-	if err != nil {
-		log.Printf("❌ Initialize the zipper stream on %s failure with err: %v", zipperAddr, err)
-		return
-	}
-
-	zipperStream = _stream
-
-	log.Printf("✅ Connected to yomo-zipper %s", zipperAddr)
 }
 
 func main() {
-	socketioServer, err := newSocketIOServer()
+	// sender will send the data to `yomo-zipper` for stream processing.
+	sender = newSender()
+
+	// receiver will receive the data from `yomo-zipper` after stream processing.
+	go setupReceiver()
+
+	// socket.io server
+	server, err := newSocketIOServer()
 	if err != nil {
 		log.Printf("❌ Initialize the socket.io server failure with err: %v", err)
 		return
 	}
 
-	// sink server which will receive the data from `yomo-sink`.
-	go serveSinkServer(socketioServer, sinkServerAddr)
+	socketioServer = server
 
 	// serve socket.io server.
-	go socketioServer.Serve()
-	defer socketioServer.Close()
+	go server.Serve()
+	defer server.Close()
 
 	router := gin.New()
 	router.Use(ginMiddleware())
-	router.GET("/socket.io/*any", gin.WrapH(socketioServer))
-	router.POST("/socket.io/*any", gin.WrapH(socketioServer))
+	router.GET("/socket.io/*any", gin.WrapH(server))
+	router.POST("/socket.io/*any", gin.WrapH(server))
 	router.Run(socketioAddr)
 
 	log.Print("✅ Serving socket.io on ", socketioAddr)
@@ -86,6 +104,99 @@ func main() {
 	}
 }
 
+var senderStream io.Writer = nil
+
+// sender sends the movement data to yomo-zipper for stream processing.
+func newSender() *Sender {
+	cli, err := client.NewSource("Socket.io Client").Connect(host, port)
+	if err != nil {
+		log.Printf("❌ Connect to the zipper server on %s failure with err: %v", zipperAddr, err)
+		return nil
+	}
+	defer cli.Close()
+
+	return &Sender{
+		Stream: cli,
+	}
+}
+
+// eventDataKey represents the Tag of a Y3 encoded data packet.
+const eventDataKey = 0x10
+
+// init a new Y3 codec.
+var codec = y3.NewCodec(eventDataKey)
+
+// send the data to `yomo-zipper`.
+func (s *Sender) send(data EventData) {
+	if s.Stream == nil {
+		return
+	}
+
+	// encode the data via Y3 Codec.
+	buf, _ := codec.Marshal(data)
+	// send the encoded data to `yomo-zipper`.
+	_, err := s.Stream.Write(buf)
+
+	if err != nil {
+		log.Printf("❌ Send to the zipperStream failure with err: %v", err)
+	}
+}
+
+// setupReceiver connects to `yomo-zipper` as a `yomo-sink`.
+// receiver will receive the data from yomo-zipper after stream processing and broadcast it to socket.io clients.
+func setupReceiver() {
+	cli, err := client.NewServerless("Socket.io").Connect(host, port)
+	if err != nil {
+		log.Printf("❌ Connect to the zipper server on %s failure with err: %v", zipperAddr, err)
+		return
+	}
+
+	defer cli.Close()
+	cli.Pipe(receiverHandler)
+}
+
+var decode = func(v []byte) (interface{}, error) {
+	var mold EventData
+	err := y3.ToObject(v, &mold)
+	if err != nil {
+		return nil, err
+	}
+	return mold, nil
+}
+
+var broadcastEventToUsers = func(_ context.Context, i interface{}) (interface{}, error) {
+	x, ok := i.(EventData)
+	if !ok {
+		err := fmt.Sprintf("expected type 'EventData', got '%v' instead",
+			reflect.TypeOf(i))
+		log.Printf("❌ %v", err)
+		return nil, errors.New(err)
+	}
+
+	if x.Event == "move" {
+		updatePlayerMovement(x)
+	}
+
+	// broadcast message to all connected user.
+	if socketioServer != nil {
+		socketioServer.BroadcastToRoom("", x.Room, x.Event, x.Data)
+	}
+
+	return i, nil
+}
+
+// receiverHandler will handle data in Rx way
+func receiverHandler(rxstream rx.RxStream) rx.RxStream {
+	stream := rxstream.
+		Subscribe(eventDataKey).
+		OnObserve(decode).
+		Map(broadcastEventToUsers).
+		Marshal(json.Marshal)
+
+	return stream
+}
+
+// newSocketIOServer creates a new socket.io server.
 func newSocketIOServer() (*socketio.Server, error) {
 	log.Print("Starting socket.io server...")
 	server, err := socketio.NewServer(nil)
@@ -96,7 +207,7 @@ func newSocketIOServer() (*socketio.Server, error) {
 	// add all connected user to the room "yomo-demo".
 	server.OnConnect("/", func(s socketio.Conn) error {
 		s.SetContext("")
-		log.Print("connected:", s.ID())
+		log.Print("connected: ", getPlayerID(s))
 		s.Join(socketioRoom)
 
 		return nil
@@ -104,69 +215,124 @@ func newSocketIOServer() (*socketio.Server, error) {
 
 	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
 		server.LeaveRoom("/", socketioRoom, s)
-		if players[s.ID()].ID != "" {
-			players[s.ID()] = Player{}
+		playerID := getPlayerID(s)
+		if players[playerID].ID != "" {
+			delete(players, playerID)
 		}
-		server.BroadcastToRoom("", socketioRoom, "leave", s.ID())
+
+		if sender != nil {
+			// send event data to `yomo-zipper` for broadcasting to geo-distributed users.
+			sender.send(EventData{
+				Region: region,
+				Room:   socketioRoom,
+				Event:  "leave",
+				Data:   playerID,
+			})
+		} else {
+			// if the sender is nil, broadcast the data to users via socket.io directly.
+			server.BroadcastToRoom("", socketioRoom, "leave", playerID)
+		}
 	})
 
 	server.OnEvent("/", "join", func(s socketio.Conn, msg string) {
 		var player Player
 
+		// unmarshal the player from socket.io msg.
 		json.Unmarshal([]byte(msg), &player)
-		id := player.ID
-		player.ID = s.ID()
-		if id == "" {
-			players[player.ID] = player
+		if player.ID == "" {
+			player.ID = getPlayerID(s)
+		}
+
+		// add player to list.
+		players[player.ID] = player
+
+		// marshal the player for broadcasting.
+		newPlayerData, _ := json.Marshal(player)
+		if sender != nil {
+			// send event data to `yomo-zipper` for broadcasting to geo-distributed users.
+			sender.send(EventData{
+				Region: region,
+				Room:   socketioRoom,
+				Event:  "join",
+				Data:   string(newPlayerData),
+			})
 		} else {
-			players[id] = player
+			// if the sender is nil, broadcast the data to users via socket.io directly.
+			server.BroadcastToRoom("", socketioRoom, "join", string(newPlayerData))
 		}
 
-		newplayer, _ := json.Marshal(player)
-		server.BroadcastToRoom("", socketioRoom, "join", string(newplayer))
+		broadcastCurrentPlayers(server)
 
-		current := make([]Player, 0)
-
-		for _, v := range players {
-			current = append(current, v)
-		}
-		allplayer, _ := json.Marshal(current)
-		server.BroadcastToRoom("", socketioRoom, "current", string(allplayer))
+		// broadcast server region
+		server.BroadcastToRoom("", socketioRoom, "region", region)
 	})
 
 	server.OnEvent("/", "current", func(s socketio.Conn, msg string) {
-		current := make([]Player, 0)
-
-		for _, v := range players {
-			current = append(current, v)
-		}
-		allplayer, _ := json.Marshal(current)
-		server.BroadcastToRoom("", socketioRoom, "current", string(allplayer))
+		broadcastCurrentPlayers(server)
 	})
 
-	server.OnEvent("/", "move", func(s socketio.Conn, key string) {
-		proto := y3.NewCodec(0x10)
-		sendingBuf, _ := proto.Marshal(key)
-		write2zipper(sendingBuf)
+	server.OnEvent("/", "move", func(s socketio.Conn, movement string) {
+		if sender != nil {
+			data := EventData{
+				Region: region,
+				Room:   socketioRoom,
+				Event:  "move",
+				Data:   movement,
+			}
+			sender.send(data)
+		}
 	})
 
 	return server, nil
 }
 
-// serveSinkServer serves the Sink server over QUIC.
-func serveSinkServer(socketioServer *socketio.Server, addr string) {
-	log.Print("Starting sink server...")
-	handler := &quicServerHandler{
-		socketioServer,
-	}
-	quicServer := quic.NewServer(handler)
+func getPlayerID(s socketio.Conn) string {
+	// return fmt.Sprint(region, "-", s.ID())
+	return s.ID()
+}
 
-	err := quicServer.ListenAndServe(context.Background(), addr)
-	if err != nil {
-		log.Printf("❌ Serve the sink server on %s failure with err: %v", addr, err)
+// broadcast current players to all geo-distributed users.
+func broadcastCurrentPlayers(server *socketio.Server) {
+	current := make([]Player, 0)
+
+	for _, v := range players {
+		current = append(current, v)
+	}
+	allPlayers, _ := json.Marshal(current)
+
+	if sender != nil {
+		// send event data to `yomo-zipper` for broadcasting to geo-distributed users.
+		sender.send(EventData{
+			Region: region,
+			Room:   socketioRoom,
+			Event:  "current",
+			Data:   string(allPlayers),
+		})
+	} else {
+		server.BroadcastToRoom("", socketioRoom, "current", string(allPlayers))
 	}
 }
 
+// updatePlayerMovement updates the movement action in players list.
+func updatePlayerMovement(x EventData) {
+	var action Action
+
+	err := json.Unmarshal([]byte(x.Data), &action)
+	if err != nil {
+		log.Printf("❌ Unmarshal the movement action failed: %v", err)
+		return
+	}
+
+	p := players[action.ID]
+	players[action.ID] = Player{
+		ID:   action.ID,
+		Name: p.Name,
+		X:    action.X,
+		Y:    action.Y,
+	}
+}
+
+// ginMiddleware allows the CORS in socket.io server.
 func ginMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		requestOrigin := c.Request.Header.Get("Origin")
@@ -184,59 +350,5 @@ func ginMiddleware() gin.HandlerFunc {
 		c.Request.Header.Del("Origin")
 
 		c.Next()
-	}
-}
-
-type quicServerHandler struct {
-	socketioServer *socketio.Server
-}
-
-func (s *quicServerHandler) Listen() error {
-	// you can add the customized codes which will be triggered when QUIC server is listening.
-	return nil
-}
-
-func (s *quicServerHandler) Read(st quic.Stream) error {
-	// receive the data from `yomo-flow` and use rx (ReactiveX) to process the stream.
-	var callback = func(v []byte) (interface{}, error) {
-		return y3.ToUTF8String(v)
-	}
-
-	rxStream := rx.FromReaderWithY3(st).Subscribe(0x10).OnObserve(callback)
-
-	go func() {
-		for customer := range rxStream.Observe() {
-			if customer.Error() {
-				log.Print(customer.E.Error())
-			} else if customer.V != nil {
-				// broadcast message to all connected user.
-				var action Action
-
-				json.Unmarshal([]byte(customer.V.(string)), &action)
-				p := players[action.ID]
-
-				players[action.ID] = Player{
-					ID:   action.ID,
-					Name: p.Name,
-					X:    action.X,
-					Y:    action.Y,
-				}
-				s.socketioServer.BroadcastToRoom("", socketioRoom, "move", customer.V.(string))
-			}
-		}
-	}()
-
-	return nil
-}
-
-func write2zipper(data []byte) {
-	if zipperStream == nil {
-		return
-	}
-
-	_, err := zipperStream.Write(data)
-
-	if err != nil {
-		log.Printf("❌ Send to the zipperStream failure with err: %v", err)
 	}
 }
